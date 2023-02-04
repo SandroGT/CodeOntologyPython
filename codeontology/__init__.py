b86350548f66b0b1132d2f4a3a806dbe6c2b5e39
Sandro Gabriele Tiddia <sandrogabrieletiddia@outlook.it>
2023-02-04 17:39:55 +0100

    Added possibility to extend the recursion limit

--- codeontology/__init__.py
+++ codeontology/__init__.py
@@ -1,16 +1,20 @@
 """A tool and ontology to model object-oriented programming languages."""
 
 LOGGER = None
+RECURSION_LIMIT = 1000
 
 
 def initialize():
     import logging
     from logging.config import fileConfig
     from pathlib import Path
+    import sys
+
     global LOGGER
 
     fileConfig(Path(__file__).parent.joinpath("logging.ini"))
     LOGGER = logging.getLogger()
+    sys.setrecursionlimit(RECURSION_LIMIT)
 
 
 initialize()
