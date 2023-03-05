4187b915a213a22159631ec2ae7d9fec29de7f3c
Sandro Gabriele Tiddia <sandrogabrieletiddia@outlook.it>
2023-03-05 14:13:13 +0100

    Added nodes colors to queries results depending on deep level

--- codeontology/rdfization/python3/jupyter_experiments/query.py
+++ codeontology/rdfization/python3/jupyter_experiments/query.py
@@ -18,7 +18,7 @@ def list_query_results(query: str, max_res=20):
             to_print = f"{line_n} {tuple_iri_res}"
             if len(to_print) > max_line_len and len(tuple_iri_res) > 1:
                 print(f"{' ' * (n - len(i_str))}{i_str} (")
-                print("\n".join([f"{' ' * n}      {r}" for r in res]), end="\n,")
+                print("\n".join([f"{' ' * n}      {r}" for r in tuple_iri_res]), end="\n,")
                 print(f"{' ' * n} )")
             else:
                 print(to_print)
@@ -29,6 +29,9 @@ def list_query_results(query: str, max_res=20):
 
 
 def show_subgraph_from_entity(iri: str, ontology, w, h, max_deep=2, max_str_len=30):
+    colors = ["#F0A3FF", "#0075DC", "#993F00", "#2BCE48", "#FFCC99", "#94FFB5", "#8F7C00", "#9DCC00", "#C20088",
+              "#FFA405", "#FFA8BB", "#FF0010", "#5EF1F2", "#00998F", "#E0FF66", "#740AFF", "#FFFF80", "#FF5005"]
+
     def get_id(e_):
         return e_.iri if hasattr(e_, "iri") else str(e_)
 
@@ -58,7 +61,7 @@ def show_subgraph_from_entity(iri: str, ontology, w, h, max_deep=2, max_str_len=
         assert deep >= 0
         start_entity = list(ontology.search(iri=start_iri))[0]
 
-        cumulated_entities = {(start_entity, type(start_entity),)}
+        cumulated_entities = {(start_entity, type(start_entity), 0)}
         cumulated_triples = set()
         iris_complete = {start_iri}
         iris_by_deep = [{start_iri}]
@@ -83,12 +86,17 @@ def show_subgraph_from_entity(iri: str, ontology, w, h, max_deep=2, max_str_len=
                     for k in [0, 2]:
                         if triple[k] is None:
                             triple[k] = entity
-                        if hasattr(triple[k], "iri") and not triple[k].iri in iris_complete:
-                            iris_complete.add(triple[k].iri)
-                            iris_by_deep[j].add(triple[k].iri)
-                        cumulated_entities.add(
-                            (triple[k], type(triple[k]),)
-                        )
+                        if hasattr(triple[k], "iri"):
+                            if not triple[k].iri in iris_complete:
+                                iris_complete.add(triple[k].iri)
+                                iris_by_deep[j].add(triple[k].iri)
+                                cumulated_entities.add(
+                                    (triple[k], type(triple[k]), j)
+                                )
+                        else:
+                            cumulated_entities.add(
+                                (triple[k], type(triple[k]), j)
+                            )
                     cumulated_triples.add((
                         (triple[0], type(triple[0]),),
                         triple[1],
@@ -100,8 +108,8 @@ def show_subgraph_from_entity(iri: str, ontology, w, h, max_deep=2, max_str_len=
     entities, triples = get_entities_and_triples(iri, max_deep)
 
     net = Network(directed=True, width=f"{w}px", height=f"{h}px")
-    for e, t_e in entities:
-        net.add_node(get_id(e), label=get_label(e, 0), shape='circle')
+    for e, t_e, c_e in entities:
+        net.add_node(get_id(e), label=f"{get_label(e, 0)}\nDeep {c_e}", shape='circle', color=colors[c_e])
     for ((subj, t_subj,), pred, (obj, t_obj,)) in triples:
         net.add_edge(get_id(subj), get_id(obj), label=get_label(pred, 1))
     net.repulsion(
